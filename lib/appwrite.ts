683f8b6b52ce37729f5c987e1a859e2c8d188c78
id alex <idalex@MacBook-Air-Pungn.local>
2026-05-24 18:21:49 +0300

    Connect to Appwrite

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,6 +2,6 @@ import { Client, Databases } from 'appwrite';
 
 const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('6a11e51d001553538418');
+    .setProject('6a11e51d001553538418');   // Твой Project ID
 
 export const databases = new Databases(client);
