3806b66196991b35e6d20bb153fa84d2dbb60a68
id alex <idalex@MacBook-Air-Pungn.local>
2026-05-25 22:39:56 +0300

    Fix Appwrite setup

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,7 +1,10 @@
-import { Client, Databases } from 'appwrite';
+import { Client, Databases, ID, Query } from 'appwrite';
 
 const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('6a11e51d001553538418');   // Твой Project ID
+    .setProject('6a11e51d001553538418');
 
 export const databases = new Databases(client);
+
+// Экспортируем нужные утилиты
+export { ID, Query };
