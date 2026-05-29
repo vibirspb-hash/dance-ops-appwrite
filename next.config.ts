fb7556998218e183977cf6f1184c5505fda846d2
id alex <idalex@MacBook-Air-Pungn.local>
2026-05-29 22:30:07 +0300

    Fix turbopack root for Render

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Явно указываем корень проекта
+  turbopack: {
+    root: '.'
+  },
+};
 
 export default nextConfig;
