d4c1a91632dc5e76a0e2a37c69cf799611b74128
id alex <idalex@MacBook-Air-Pungn.local>
2026-05-29 22:35:47 +0300

    Force standalone + webpack for Render

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'standalone',
+  webpack: (config) => {
+    return config;
+  },
 };
 
 export default nextConfig;
