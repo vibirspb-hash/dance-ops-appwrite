32c515e5118c08bde974c4d6ad1e02cf11a320fa
vibirspb-hash <vibirspb@gmail.com>
2026-05-29 15:48:54 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: { unoptimized: true },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "standalone"
 };
 
 export default nextConfig;
